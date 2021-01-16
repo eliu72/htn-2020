c180b46a5aba71269a4dbb299df55cdd3da6a467
Elaine Liu <57732643+eliu72@users.noreply.github.com>
2021-01-16 12:59:55 -0500

    frontend

---
+++ dating-trivia/src/card.js
@@ -0,0 +1,41 @@
+import React from "react";
+import { Button } from "./button";
+
+export default class Card extends React.Component {
+    constructor( props ){
+        super(props);
+    }
+
+    render (){
+        const {
+            question,
+            correctAnswer,
+            a1,
+            a2,
+            a3,
+            a4,
+        } = this.props;
+        
+        // array of answers
+        const answers = [a1].concat(a2).concat(a3).concat(a4);
+        
+        return (
+            <article className="card">
+                <div>
+                    <h1>{question}</h1>
+                </div>
+                <footer>
+                    {answers.map((answer, i) => {
+                        return (
+                        <Button
+                            option={answer}
+                        >
+                            
+                        </Button>
+                        );
+                    })}
+                </footer>
+            </article>
+        )
+    }
+}
